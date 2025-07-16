5d0d4be9872d8b4dc9b9582ab3ff48d68f14b4fe
zainab98alrez <zainab.98ta@gmail.com>
2025-07-16 23:47:44 +0300

    about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,42 +15,38 @@
     <!-- Page Header End -->
 
 
-    <!-- About Start -->
-    <div class="container-fluid overflow-hidden py-5 px-lg-0">
+     <!-- About Start -->
+     <div class="container-fluid overflow-hidden py-5 px-lg-0">
         <div class="container about py-5 px-lg-0">
             <div class="row g-5 mx-lg-0">
                 <div class="col-lg-6 ps-lg-0 wow fadeInLeft" data-wow-delay="0.1s" style="min-height: 400px;">
                     <div class="position-relative h-100">
-                        <img class="position-absolute img-fluid w-100 h-100" src="img/about.jpg" style="object-fit: cover;" alt="">
+                        <img loading="lazy" width="732" height="548" class="position-absolute img-fluid w-100 h-100" src="img/img-1.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
                     </div>
                 </div>
                 <div class="col-lg-6 about-text wow fadeInUp" data-wow-delay="0.3s">
                     <h6 class="text-dark text-uppercase mb-3">About Us</h6>
-                    <h1 class="mb-5">WELCOME TO BETA LOGISTICS</h1>
+                    <h2 class="mb-5 h1">Welcome to Beta Logistics FZE</h2>
                     <p class="mb-3">
-                        As international freight forwarders, our mission is
-                        to provide world-class logistic service to exporters
-                        and importers. We treat each shipment as a unique
-                        challenge and opportunity, identifying and providing
-                        the optimal solution for our customers specific
-                        international transportation needs
+                        Beta Logistics FZE, headquartered in Dubai, is a dynamic international logistics
+                        company committed to delivering world-class logistics and supply chain solutions with
+                        precision, reliability, and integrity.
                     </p>
                     <p class="mb-3">
-                        At BETA, we are aware of the critical importance of
-                        each shipment and how much it could mean to the
-                        daily operations of your business. We bear in mind
-                        the gravity of trust you place in us and strive to move
-                        your cargo swiftly and efficiently straight to the
-                        destination
+                        As experienced international freight forwarders, our mission is to provide exporters and
+                        importers with top-tier logistics services. Every shipment we handle is approached as a
+                        unique challenge and an opportunity to deliver excellence. We take pride in identifying
+                        and implementing the most optimal transportation solutions tailored to each client's
+                        specific needs.
                     </p>
                     <p class="mb-3">
-                        Our upskilled team of professionals uses the best
-                        resources and the latest technologies to transport
-                        your shipment across the globe carefully, and
-                        consistently
+                      At BETA, we understand the critical role logistics plays in your business. We recognize
+                        the trust you place in us, and we honor that trust by ensuring your cargo moves swiftly,
+                        safely, and efficiently to its destination. Whether it's a single shipment or a
+                        comprehensive supply chain solution, we treat every project with urgency and care.
                     </p>
 
-                    <a href="/" class="btn btn-dark rounded-1 py-3 px-5">Explore More</a>
+                    <a href="/about" class="btn btn-dark rounded-1 py-3 px-5">Explore More</a>
                 </div>
             </div>
         </div>
@@ -65,24 +61,41 @@
                     <h6 class="text-dark text-uppercase mb-3">Our Features</h6>
                     <h1 class="mb-5">We Are Trusted Logistics Company Since 1990</h1>
                     <div class="d-flex mb-5 wow fadeInUp" data-wow-delay="0.3s">
-                        <i class="fa fa-globe text-primary fa-3x flex-shrink-0"></i>
+                        <i class="fa fa-bullseye  text-primary fa-3x flex-shrink-0"></i>
                         <div class="ms-4">
-                            <h5>Worldwide Service</h5>
-                            <p class="mb-0">Diam dolor ipsum sit amet eos erat ipsum lorem sed stet lorem sit clita duo justo magna erat amet</p>
+                            <h5>Our Mission </h5>
+                            <p class="mb-0">To offer cost-effective, efficient, reliable, and fully customized logistics solutions tailored 
+                                to meet the diverse demands of our clients across various industries. From end-to-end 
+                                freight forwarding to seamless procurement, sourcing, and distribution, we provide 
+                                dependable service at every step of the journey.
+                            </p>
                         </div>
                     </div>
                     <div class="d-flex mb-5 wow fadeIn" data-wow-delay="0.5s">
-                        <i class="fa fa-shipping-fast text-primary fa-3x flex-shrink-0"></i>
+                        <i class="fa fa-eye text-primary fa-3x flex-shrink-0"></i>
                         <div class="ms-4">
-                            <h5>On Time Delivery</h5>
-                            <p class="mb-0">Diam dolor ipsum sit amet eos erat ipsum lorem sed stet lorem sit clita duo justo magna erat amet</p>
+                            <h5>Our Vision</h5>
+                            <p class="mb-0">To be a global logistics leader known for excellence, transparency, and innovation in 
+                                every shipment we handle.
+                            </p>
                         </div>
                     </div>
                     <div class="d-flex mb-0 wow fadeInUp" data-wow-delay="0.7s">
-                        <i class="fa fa-headphones text-primary fa-3x flex-shrink-0"></i>
+                        <i class="fa fa-users text-primary fa-3x flex-shrink-0"></i>
                         <div class="ms-4">
-                            <h5>24/7 Telephone Support</h5>
-                            <p class="mb-0">Diam dolor ipsum sit amet eos erat ipsum lorem sed stet lorem sit clita duo justo magna erat amet</p>
+                            <h5>WHY DO YOU CHOOSE US?</h5>
+                            <p class="mb-0">
+                                <ul>
+                                    <li>Experts in Procurement & Distribution. </li>
+                                    <li>Experts in all Freight Forwarding activities. </li>
+                                    <li>Comprehensive Service provided, right from freight, logistics, 
+                                        custom clearance, warehousing & transportation. </li>
+                                    <li>Specialist in movement of mobile phones, Watches, Electronics & 
+                                       High Value goods. </li>
+                                    <li> Security monitored office & warehouse. </li>
+                                    <li>Collective expertise of over a decade.</li>
+                                </ul>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -100,8 +113,11 @@
     <div class="container-xxl py-5">
         <div class="container py-5">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="text-dark text-uppercase">Our Team</h6>
-                <h1 class="mb-5">Expert Team Members</h1>
+                <h6 class="text-dark text-uppercase">Meet our team</h6>
+                <p class="mb-0">Our team of highly skilled professionals combines deep industry expertise with the latest 
+                    technologies and best-in-class resources. This enables us to manage global shipments 
+                    with precision, consistency, and accountability.
+                </p>
             </div>
             <div class="row g-4">
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
@@ -109,14 +125,13 @@
                         <div class="overflow-hidden mb-4">
                             <img class="img-fluid" src="img/team-1.jpg" alt="">
                         </div>
-                        <h5 class="mb-0">Full Name</h5>
-                        <p>Designation</p>
+                        <h5 class="mb-0">Fadi Saeed Al Abdullah</h5>
+                        <p>Managing Director </p>
                         <div class="btn-slide mt-1">
                             <i class="fa fa-share"></i>
                             <span>
-                                <a href=""><i class="fab fa-facebook-f"></i></a>
-                                <a href=""><i class="fab fa-twitter"></i></a>
-                                <a href=""><i class="fab fa-instagram"></i></a>
+                                <a href="https://www.linkedin.com/in/fadi-al-abdullah-15a829201/"><i class="fab fa-instagram"></i></a>
+                                <a href="fadi.alabdullah@betafzco.com"><i class="fab fa-instagram"></i></a>
                             </span>
                         </div>
                     </div>
