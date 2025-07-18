fbf28b66cbe4f1f96164497d08c1bfe5ddb6d817
zainab98alrez <zainab.98ta@gmail.com>
2025-07-18 16:45:45 +0300

    about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -80,24 +80,6 @@
                             </p>
                         </div>
                     </div>
-                    <div class="d-flex mb-0 wow fadeInUp" data-wow-delay="0.7s">
-                        <i class="fa fa-users text-primary fa-3x flex-shrink-0"></i>
-                        <div class="ms-4">
-                            <h5>WHY DO YOU CHOOSE US?</h5>
-                            <p class="mb-0">
-                                <ul>
-                                    <li>Experts in Procurement & Distribution. </li>
-                                    <li>Experts in all Freight Forwarding activities. </li>
-                                    <li>Comprehensive Service provided, right from freight, logistics,
-                                        custom clearance, warehousing & transportation. </li>
-                                    <li>Specialist in movement of mobile phones, Watches, Electronics &
-                                       High Value goods. </li>
-                                    <li> Security monitored office & warehouse. </li>
-                                    <li>Collective expertise of over a decade.</li>
-                                </ul>
-                            </p>
-                        </div>
-                    </div>
                 </div>
                 <div class="col-lg-6 pe-lg-0 wow fadeInRight" data-wow-delay="0.1s" style="min-height: 400px;">
                     <div class="position-relative h-100">
@@ -109,82 +91,86 @@
     </div>
     <!-- Feature End -->
 
+
+     <!-- WHY DO YOU CHOOSE US?Start -->
+     <div class="container-fluid overflow-hidden py-5 px-lg-0">
+        <div class="container about py-5 px-lg-0">
+            <div class="row g-5 mx-lg-0">
+                <div class="col-lg-6 ps-lg-0 wow fadeInLeft" data-wow-delay="0.1s" style="min-height: 400px;">
+                    <div class="position-relative h-100">
+                        <img loading="lazy" width="732" height="548" class="position-absolute img-fluid w-100 h-100" src="img/img-4.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
+                    </div>
+                </div>
+                <div class="col-lg-6 about-text wow fadeInUp" data-wow-delay="0.3s">
+                    <h2 class="mb-5 h1">WHY DO YOU CHOOSE US?</h2>
+                    <p class="mb-0">
+                        <ul>
+                            <li>Experts in Procurement & Distribution. </li>
+                            <li>Experts in all Freight Forwarding activities. </li>
+                            <li>Comprehensive Service provided, right from freight, logistics,
+                                custom clearance, warehousing & transportation. </li>
+                            <li>Specialist in movement of mobile phones, Watches, Electronics &
+                               High Value goods. </li>
+                            <li> Security monitored office & warehouse. </li>
+                            <li>Collective expertise of over a decade.</li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- WHY DO YOU CHOOSE US? End -->
+
     <!-- Team Start -->
     <div class="container-xxl py-5">
         <div class="container py-5">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="text-dark text-uppercase">Meet our team</h6>
-                <p class="mb-0">Our team of highly skilled professionals combines deep industry expertise with the latest
+                <h6 class="text-dark text-uppercase mb-3">Meet our team</h6>
+                <p class="mb-5">Our team of highly skilled professionals combines deep industry expertise with the latest
                     technologies and best-in-class resources. This enables us to manage global shipments
                     with precision, consistency, and accountability.
                 </p>
             </div>
             <div class="row g-4">
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                     <div class="team-item p-4">
                         <div class="overflow-hidden mb-4">
                             <img class="img-fluid" src="img/team-1.jpg" alt="">
                         </div>
                         <h5 class="mb-0">Fadi Saeed Al Abdullah</h5>
                         <p>Managing Director </p>
-                        <div class="btn-slide mt-1">
+                        {{-- <div class="btn-slide mt-1">
                             <i class="fa fa-share"></i>
                             <span>
                                 <a href="https://www.linkedin.com/in/fadi-al-abdullah-15a829201/"><i class="fab fa-instagram"></i></a>
                                 <a href="fadi.alabdullah@betafzco.com"><i class="fab fa-instagram"></i></a>
                             </span>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="team-item p-4">
-                        <div class="overflow-hidden mb-4">
-                            <img class="img-fluid" src="img/team-2.jpg" alt="">
-                        </div>
-                        <h5 class="mb-0">Full Name</h5>
-                        <p>Designation</p>
+                <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                        <h5 class="mb-3">Fadi Saeed Al Abdullah</h5>
+                        <p>Mr. Fadi is the visionary founder behind Beta Logistics FZE, bringing with him a deep 
+                            passion for innovation, quality, and customer-focused solutions. He established the 
+                            company in November 2022 with a clear mission: 
+                        </p>
+                        <p>To deliver excellence in every project. 
+                            Since then, under his leadership, Beta Logistics FZE has evolved from a bold idea into 
+                            a trusted name in the logistics industry, recognized for: </br>
+                            ✅ Reliable, on-time service </br>
+                            ✅ Strong commitment to ethical business values
+                        </p>
+                        <p>Mr. Fadi believes: 
+                            “Success is built on trust, and trust is earned through consistent quality and integrity.” 
+                            His dedication continues to drive the company forward, setting new benchmarks for 
+                            professionalism and customer satisfaction.
+                        </p>
                         <div class="btn-slide mt-1">
                             <i class="fa fa-share"></i>
                             <span>
-                                <a href=""><i class="fab fa-facebook-f"></i></a>
-                                <a href=""><i class="fab fa-twitter"></i></a>
-                                <a href=""><i class="fab fa-instagram"></i></a>
+                                <a href="https://www.linkedin.com/in/fadi-al-abdullah-15a829201/"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                <a href="fadi.alabdullah@betafzco.com"><i class="fa fa-envelope-open"></i></a>
                             </span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
-                    <div class="team-item p-4">
-                        <div class="overflow-hidden mb-4">
-                            <img class="img-fluid" src="img/team-3.jpg" alt="">
-                        </div>
-                        <h5 class="mb-0">Full Name</h5>
-                        <p>Designation</p>
-                        <div class="btn-slide mt-1">
-                            <i class="fa fa-share"></i>
-                            <span>
-                                <a href=""><i class="fab fa-facebook-f"></i></a>
-                                <a href=""><i class="fab fa-twitter"></i></a>
-                                <a href=""><i class="fab fa-instagram"></i></a>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.9s">
-                    <div class="team-item p-4">
-                        <div class="overflow-hidden mb-4">
-                            <img class="img-fluid" src="img/team-4.jpg" alt="">
-                        </div>
-                        <h5 class="mb-0">Full Name</h5>
-                        <p>Designation</p>
-                        <div class="btn-slide mt-1">
-                            <i class="fa fa-share"></i>
-                            <span>
-                                <a href=""><i class="fab fa-facebook-f"></i></a>
-                                <a href=""><i class="fab fa-twitter"></i></a>
-                                <a href=""><i class="fab fa-instagram"></i></a>
-                            </span>
-                        </div>
                     </div>
                 </div>
             </div>
