87ca6c9f970438fffe47f23da23de46ab50b5614
Ali Aljarrah <ali.jarrah.109@gmail.com>
2025-07-21 23:19:46 +0300

    fix image name

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,10 +1,10 @@
 @include('include.head')
- <title>Beta Logistics FZE - About us</title>
+<title>Beta Logistics FZE - About us</title>
 </head>
 
 <body>
-  @include('include.loader')
-  @include('include.menu')
+    @include('include.loader')
+    @include('include.menu')
 
     <!-- Page Header Start -->
     <div class="container-fluid page-header py-5" style="margin-bottom: 6rem;">
@@ -15,13 +15,14 @@
     <!-- Page Header End -->
 
 
-     <!-- About Start -->
-     <div class="container-fluid overflow-hidden py-5 px-lg-0">
+    <!-- About Start -->
+    <div class="container-fluid overflow-hidden py-5 px-lg-0">
         <div class="container about py-5 px-lg-0">
             <div class="row g-5 mx-lg-0">
                 <div class="col-lg-6 ps-lg-0 wow fadeInLeft" data-wow-delay="0.1s" style="min-height: 400px;">
                     <div class="position-relative h-100">
-                        <img loading="lazy" width="732" height="548" class="position-absolute img-fluid w-100 h-100" src="img/img-1.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
+                        <img loading="lazy" width="732" height="548" class="position-absolute img-fluid w-100 h-100"
+                            src="img/img-1.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
                     </div>
                 </div>
                 <div class="col-lg-6 about-text wow fadeInUp" data-wow-delay="0.3s">
@@ -40,7 +41,7 @@
                         specific needs.
                     </p>
                     <p class="mb-3">
-                      At BETA, we understand the critical role logistics plays in your business. We recognize
+                        At BETA, we understand the critical role logistics plays in your business. We recognize
                         the trust you place in us, and we honor that trust by ensuring your cargo moves swiftly,
                         safely, and efficiently to its destination. Whether it's a single shipment or a
                         comprehensive supply chain solution, we treat every project with urgency and care.
@@ -61,17 +62,19 @@
                     <h1 class="mb-5">Beta Logistics established in the year 2022</h1>
                     <div class="d-flex mb-5 wow fadeInUp" data-wow-delay="0.3s">
                         <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="#d8b768" class="bi bi-bullseye" viewBox="0 0 16 16">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                                <path d="M8 13A5 5 0 1 1 8 3a5 5 0 0 1 0 10m0 1A6 6 0 1 0 8 2a6 6 0 0 0 0 12"/>
-                                <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8"/>
-                                <path d="M9.5 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="#d8b768"
+                                class="bi bi-bullseye" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                <path d="M8 13A5 5 0 1 1 8 3a5 5 0 0 1 0 10m0 1A6 6 0 1 0 8 2a6 6 0 0 0 0 12" />
+                                <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8" />
+                                <path d="M9.5 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
                             </svg>
                         </div>
 
                         <div class="ms-4">
                             <h5>Our Mission </h5>
-                            <p class="mb-0">To offer cost-effective, efficient, reliable, and fully customized logistics solutions tailored
+                            <p class="mb-0">To offer cost-effective, efficient, reliable, and fully customized logistics
+                                solutions tailored
                                 to meet the diverse demands of our clients across various industries. From end-to-end
                                 freight forwarding to seamless procurement, sourcing, and distribution, we provide
                                 dependable service at every step of the journey.
@@ -80,14 +83,17 @@
                     </div>
                     <div class="d-flex mb-5 wow fadeIn" data-wow-delay="0.5s">
                         <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="#d8b768" class="bi bi-eye-fill" viewBox="0 0 16 16">
-                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
-                                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="#d8b768"
+                                class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
+                                <path
+                                    d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
                             </svg>
                         </div>
                         <div class="ms-4">
                             <h5>Our Vision</h5>
-                            <p class="mb-0">To be a global logistics leader known for excellence, transparency, and innovation in
+                            <p class="mb-0">To be a global logistics leader known for excellence, transparency, and
+                                innovation in
                                 every shipment we handle.
                             </p>
                         </div>
@@ -95,7 +101,8 @@
                 </div>
                 <div class="col-lg-6 pe-lg-0 wow fadeInRight" data-wow-delay="0.1s" style="min-height: 400px;">
                     <div class="position-relative h-100">
-                        <img class="position-absolute img-fluid w-100 h-100" src="img/feature.jpg" style="object-fit: cover;" alt="">
+                        <img class="position-absolute img-fluid w-100 h-100" src="img/feature.jpg"
+                            style="object-fit: cover;" alt="">
                     </div>
                 </div>
             </div>
@@ -104,13 +111,14 @@
     <!-- Feature End -->
 
 
-     <!-- WHY DO YOU CHOOSE US?Start -->
-     <div class="container-fluid overflow-hidden py-5 px-lg-0">
+    <!-- WHY DO YOU CHOOSE US?Start -->
+    <div class="container-fluid overflow-hidden py-5 px-lg-0">
         <div class="container about py-5 px-lg-0">
             <div class="row g-5 mx-lg-0">
                 <div class="col-lg-6 ps-lg-0 wow fadeInLeft" data-wow-delay="0.1s" style="min-height: 400px;">
                     <div class="position-relative h-100">
-                        <img loading="lazy" width="732" height="548" class="position-absolute img-fluid w-100 h-100" src="img/img-4.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
+                        <img loading="lazy" width="732" height="548" class="position-absolute img-fluid w-100 h-100"
+                            src="img/img-4.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
                     </div>
                 </div>
                 <div class="col-lg-6 about-text wow fadeInUp" data-wow-delay="0.3s">
@@ -136,7 +144,8 @@
         <div class="container py-5">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
                 <h2 class="mb-3 h1">Meet our team</h2>
-                <p class="mb-5">Our team of highly skilled professionals combines deep industry expertise with the latest
+                <p class="mb-5">Our team of highly skilled professionals combines deep industry expertise with the
+                    latest
                     technologies and best-in-class resources. This enables us to manage global shipments
                     with precision, consistency, and accountability.
                 </p>
@@ -144,7 +153,9 @@
             <div class="row g-4">
                 <div class="col-lg-6 ps-lg-0 wow fadeInLeft" data-wow-delay="0.1s" style="min-height: 400px;">
                     <div class="position-relative h-100">
-                        <img loading="lazy" width="732" height="548" class="position-absolute img-fluid w-100 h-100 mt-2" src="img/fadi.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
+                        <img loading="lazy" width="732" height="548"
+                            class="position-absolute img-fluid w-100 h-100 mt-2" src="img/fadi.webp"
+                            style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 wow fadeInUp px-4" data-wow-delay="0.3s">
@@ -166,14 +177,19 @@
                         professionalism and customer satisfaction.
                     </p>
                     <div class="mt-1 d-flex justify-content-start align-items-center">
-                        <a class="d-block me-4" href="https://www.linkedin.com/in/fadi-al-abdullah-15a829201/" target="_blank" rel="noopener noreferrer" aria-label="linkedin">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#000" class="bi bi-linkedin" viewBox="0 0 16 16">
-                                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+                        <a class="d-block me-4" href="https://www.linkedin.com/in/fadi-al-abdullah-15a829201/"
+                            target="_blank" rel="noopener noreferrer" aria-label="linkedin">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#000"
+                                class="bi bi-linkedin" viewBox="0 0 16 16">
+                                <path
+                                    d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                             </svg>
                         </a>
                         <a class="d-block me-4" href="mailto:fadi.alabdullah@betafzco.com">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#000" class="bi bi-envelope-fill" viewBox="0 0 16 16">
-                                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#000"
+                                class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
                             </svg>
                         </a>
                     </div>
@@ -183,66 +199,73 @@
     </section>
     <!-- Team End -->
 
-<!-- Client 2 - Bootstrap Brain Component -->
-<section class="py-3 py-md-5 py-xl-8">
-    <div class="container py-5">
-        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-            <h2 class="mb-3 h1">Our Clients</h2>
-        </div>
-    </div>
-    <div class="container overflow-hidden">
-      <div class="row">
-        <div class="col-lg-3 col-md-3 col-xl-3 text-center">
-            <div class="bg-light rounded py-9 px-3">
-                <img class="u-clients img-fluid w-100 h-100 mt-3 mb-3"  loading="lazy" width="132" height="54" src="img/clients/Alexcina.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
+    <section class="py-3 py-md-5 py-xl-8">
+        <div class="container py-5">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h2 class="mb-3 h1">Our Clients</h2>
             </div>
-            <p class="my-4 fw-medium">
-                Alexcina 
-            </p>
-        </div>
-        <div class="col-lg-3 col-md-3 col-xl-3 text-center">
-            <div class="bg-light rounded py-9 px-3">
-                <img class="img-fluid w-100 h-100 mt-3 mb-3"  loading="lazy" width="132" height="54" src="img/clients/Skywalk.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-xl-3 text-center">
+                    <div class="bg-light rounded py-9 px-3">
+                        <img class="u-clients img-fluid w-100 h-100 mt-3 mb-3" loading="lazy" width="132" height="54"
+                            src="{{asset('img/clients/Alexcina.webp')}}" style="object-fit: cover;"
+                            alt="Welcome to Beta Logistics FZE">
+                    </div>
+                    <p class="my-4 fw-medium">
+                        Alexcina
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-3 col-xl-3 text-center">
+                    <div class="bg-light rounded py-9 px-3">
+                        <img class="img-fluid w-100 h-100 mt-3 mb-3" loading="lazy" width="132" height="54"
+                            src="{{asset('img/clients/Skywalk.webp')}}" style="object-fit: cover;"
+                            alt="Welcome to Beta Logistics FZE">
+                    </div>
+                    <p class="my-4 fw-medium">
+                        Skywalk
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-3 col-xl-3 text-center">
+                    <div class="bg-light rounded py-9 px-3">
+                        <img class="img-fluid w-100 h-100 mt-3 mb-3" loading="lazy" width="132" height="30"
+                            src="{{asset('img/clients/Silkway.webp')}}" style="object-fit: cover;"
+                            alt="Welcome to Beta Logistics FZE">
+                    </div>
+                    <p class="my-4 fw-medium">
+                        Silkway
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-3 col-xl-3 text-center">
+                    <div class="bg-light rounded py-9 px-3">
+                        <img class="img-fluid w-100 h-100 mt-3 mb-3" loading="lazy" width="132" height="54"
+                            src="{{asset('img/clients/sharjah-aviation.webp')}}" style="object-fit: cover;"
+                            alt="Welcome to Beta Logistics FZE">
+                    </div>
+                    <p class="my-4 fw-medium">
+                        Sharjah Aviation
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-3 col-xl-3 text-center">
+                    <div class="bg-light rounded py-9 px-3">
+                        <img class="u-clients img-fluid w-100 h-100 mt-3 mb-3" loading="lazy" width="132" height="54"
+                            src="{{asset('img/clients/Flyme.webp')}}" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
+                    </div>
+                    <p class="my-4 fw-medium">
+                        Flyme
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-3 col-xl-3 text-center">
+                    <div class="bg-light rounded py-9 px-3">
+                        <img class="u-clients img-fluid w-100 h-100 mt-3 mb-3" loading="lazy" width="132" height="54"
+                            src="{{asset('img/clients/Camex.webp')}}" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
+                    </div>
+                    <p class="my-4 fw-medium">
+                        Camex
+                    </p>
+                </div>
             </div>
-            <p class="my-4 fw-medium">
-                Skywalk
-            </p>
         </div>
-        <div class="col-lg-3 col-md-3 col-xl-3 text-center">
-            <div class="bg-light rounded py-9 px-3">
-                <img class="img-fluid w-100 h-100 mt-3 mb-3"  loading="lazy" width="132" height="30" src="img/clients/Silkway.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
-            </div>
-            <p class="my-4 fw-medium">
-                Silkway
-            </p>
-        </div>
-        <div class="col-lg-3 col-md-3 col-xl-3 text-center">
-            <div class="bg-light rounded py-9 px-3">
-                <img class="img-fluid w-100 h-100 mt-3 mb-3"  loading="lazy" width="132" height="54" src="img/clients/sharjah-aviation.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
-            </div>
-            <p class="my-4 fw-medium">
-                Sharjah Aviation
-            </p>
-        </div>
-        <div class="col-lg-3 col-md-3 col-xl-3 text-center">
-            <div class="bg-light rounded py-9 px-3">
-                <img class="u-clients img-fluid w-100 h-100 mt-3 mb-3"  loading="lazy" width="132" height="54" src="img/clients/Flyme.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
-            </div>
-            <p class="my-4 fw-medium">
-                Flyme
-            </p>
-        </div>
-        <div class="col-lg-3 col-md-3 col-xl-3 text-center">
-            <div class="bg-light rounded py-9 px-3">
-                <img class="u-clients img-fluid w-100 h-100 mt-3 mb-3"  loading="lazy" width="132" height="54" src="img/clients/Camex.webp" style="object-fit: cover;" alt="Welcome to Beta Logistics FZE">
-            </div>
-            <p class="my-4 fw-medium">
-                Camex
-            </p>
-        </div>
-      </div>
-    </div>
-  </section>
+    </section>
 
 
 
